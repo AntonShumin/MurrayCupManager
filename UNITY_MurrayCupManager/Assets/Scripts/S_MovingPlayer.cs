9b11c5840f792016985f5989c86c03b5257e9b8f
CGPepper <AntonShuminBe@gmail.com>
2015-04-04 19:20:27 +0200

    Working on Racing

---
+++ UNITY_MurrayCupManager/Assets/Scripts/S_MovingPlayer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class S_MovingPlayer : MonoBehaviour 
+{
+	public bool pit = false;
+
+	public void ChooseSubway (ref int i,Vector3[] nextPos)
+	{
+		if (!pit) 
+			i = 1;
+		else
+			i = 0;
+	}
+
+}
