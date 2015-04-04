392f0e705883d4d4d1200cd616b5de9169e15438
CGPepper <AntonShuminBe@gmail.com>
2015-04-04 23:21:26 +0200

    racing stuff
    editor keeps resetting to .net 3.5, what a pain in the azz

--- UNITY_MurrayCupManager/Assets/Scripts/S_MovingPlayer.cs
+++ UNITY_MurrayCupManager/Assets/Scripts/S_MovingPlayer.cs
@@ -4,6 +4,9 @@ using System.Collections;
 public class S_MovingPlayer : MonoBehaviour 
 {
 	public bool pit = false;
+	public GameObject hHelper;
+	public GameObject hShip;
+	private Vector3 offset;
 
 	public void ChooseSubway (ref int i,Vector3[] nextPos)
 	{
@@ -12,5 +15,21 @@ public class S_MovingPlayer : MonoBehaviour
 		else
 			i = 0;
 	}
+
+	void Awake()
+	{
+		hHelper = gameObject;
+		offset =  transform.position - hShip.transform.position;
+	}
+
+	//void LateUpdate()
+	void nothing()
+	{
+		Vector3 _position = transform.position;
+		hShip.transform.position = _position + offset;
+		hShip.transform.LookAt (_position);
+		//transform.localPosition.y += 10;
+	}
+
 
 }
